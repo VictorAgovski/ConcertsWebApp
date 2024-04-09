8aeee3df1b80e1acdafd39aa6323e4e57d763c0a
Victor Agovski <122831850+VictorAgovski@users.noreply.github.com>
2024-04-09 07:10:54 +0300

    Add SeedData class

---
+++ LiveMetal.Infrastructure/Data/SeedDb/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using LiveMetal.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveMetal.Infrastructure.Data.SeedDb
+{
+    internal class SeedData
+    {
+        public ApplicationUser GuestUser { get; set; }
+
+        public ApplicationUser AdminUser { get; set; }
+
+        public ApplicationUser RegisteredUser { get; set; }
+
+        public IdentityUserClaim<string> GuestUserClaim { get; set; }
+
+        public IdentityUserClaim<string> AdminUserClaim { get; set; }
+
+        public IdentityUserClaim<string> RegisteredUserClaim { get; set; }
+
+
+    }
+}
