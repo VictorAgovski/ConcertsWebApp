e8455cb3dfd75cc62ecff82e7a87479079b23b89
Victor Agovski <122831850+VictorAgovski@users.noreply.github.com>
2024-04-16 00:19:40 +0300

    Error Views Added

--- LiveMetal/Controllers/HomeController.cs
+++ LiveMetal/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 ﻿using LiveMetal.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LiveMetal.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -18,15 +19,22 @@ namespace LiveMetal.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
