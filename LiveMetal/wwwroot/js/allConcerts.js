bf7263171aa3c986bb1762df23f24136742bbfe2
Victor Agovski <122831850+VictorAgovski@users.noreply.github.com>
2024-04-21 01:31:07 +0300

    Solving the problem with the arrows

--- LiveMetal/wwwroot/js/allConcerts.js
+++ LiveMetal/wwwroot/js/allConcerts.js
@@ -4,14 +4,9 @@
             var carouselId = this.getAttribute('data-target');
             var carousel = document.getElementById(carouselId.slice(1));
             var carouselInner = carousel.querySelector('.review-carousel');
-            if (carousel.style.display === 'none' || !carousel.style.display) {
-                carousel.style.display = 'block';
-                carouselInner.style.transform = 'translateX(0)'; // Reset to the first slide
-                this.textContent = "Hide Reviews";
-            } else {
-                carousel.style.display = 'none';
-                this.textContent = "Show Reviews";
-            }
+            carousel.style.display = (carousel.style.display === 'none' || !carousel.style.display) ? 'block' : 'none';
+            this.textContent = (carousel.style.display === 'block') ? "Hide Reviews" : "Show Reviews";
+            carouselInner.style.transform = 'translateX(0px)'; // Reset to the first review
         });
     });
 });
@@ -22,15 +17,12 @@ function moveSlide(concertId, direction) {
         var carousel = container.querySelector('.review-carousel');
         var slides = carousel.querySelectorAll('.carousel-slide');
         if (slides.length > 1) {
-            var slideWidth = container.clientWidth; // Make sure this calculation is correct
+            var slideWidth = container.clientWidth;
             var currentTransform = getComputedStyle(carousel).transform;
             var currentIndex = currentTransform === 'none' ? 0 : parseInt(currentTransform.split(',')[4].trim());
             var newIndex = currentIndex - (slideWidth * direction);
-
-            // Clamping the newIndex to ensure it doesn't go out of bounds
             newIndex = Math.max(newIndex, -(slides.length - 1) * slideWidth);
             newIndex = Math.min(newIndex, 0);
-
             carousel.style.transform = `translateX(${newIndex}px)`;
         }
     }
